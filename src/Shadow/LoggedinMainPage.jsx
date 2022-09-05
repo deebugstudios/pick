b447e801dab0095674c5eb4afc5b09eca0224f9d
Chinedu <nanagwonye@gmail.com>
2022-09-05 16:48:09 +0100

    28th commit

--- src/Shadow/LoggedinMainPage.jsx
+++ src/Shadow/LoggedinMainPage.jsx
@@ -9,28 +9,27 @@ import { Navigate } from "react-router-dom";
 import { RiderContext } from "./Pages/Contexts/RiderContext";
 const LoggedinMainPage = (props) => {
   const [loggedin, setLoggedin] = useState(props.logged);
-  const [sideBar, setSideBar ]= useState(false)
-  
+  const [sideBar, setSideBar] = useState(false);
+
   const value = useContext(RiderContext);
   const { token } = value;
 
-
   const toggleSideBar = () => {
-    setSideBar(!sideBar)
-  }
+    setSideBar(!sideBar);
+  };
   return (
     <section className="user-dashboard-main">
       <div className="user-left-side-main">
-        {token ? <Navsignedin siderBar={toggleSideBar}/> : <NavigationBar />}
+        {token ? <Navsignedin siderBar={toggleSideBar} /> : <NavigationBar />}
       </div>
       <div className="user-right-side-main">
         {token ? (
-          <div className={sideBar? "sider-group-active":"sider-groups"}>
-            <SideBar toggle={sideBar} toggler= {toggleSideBar}/>{" "}
+          <div className={sideBar ? "sider-group-active" : "sider-groups"}>
+            <SideBar toggle={sideBar} toggler={toggleSideBar} />{" "}
           </div>
         ) : null}
 
-      {token ? <div className="map-container1">{props.name}</div> : <Navigate to = "/main1"/>  }  
+        <div className="map-container1">{props.name}</div>
       </div>
     </section>
   );
