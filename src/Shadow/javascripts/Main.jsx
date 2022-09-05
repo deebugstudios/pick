ba7a1edc346ce48f59640d690e747367bde323ed
Chinedu <nanagwonye@gmail.com>
2022-09-05 17:06:41 +0100

    29th commit

--- src/Shadow/javascripts/Main.jsx
+++ src/Shadow/javascripts/Main.jsx
@@ -15,63 +15,65 @@ import { Autocomplete } from "@react-google-maps/api";
 
 const Main = () => {
   return (
-    <section className="main-first">
-      <div className="scroll-main">
-        <img src={reddots} alt="" className="red-dots" />
-        <img src={greendots} alt="" className="green-dots" />
-        <div className="main-container" style={{backgroundColor:"white"}}>
-          <div className="main-left-side">
-            <div className="left-side-container">
-              <div className="header-text">
-                <h3 className="main-text">
-                  Door to Door <span>delivery</span> services for individuals
-                  and businesses
-                </h3>
+    <div className="wrapper-main">
+      <section className="main-first">
+        <div className="scroll-main">
+          <img src={reddots} alt="" className="red-dots" />
+          <img src={greendots} alt="" className="green-dots" />
+          <div className="main-container" style={{ backgroundColor: "white" }}>
+            <div className="main-left-side">
+              <div className="left-side-container">
+                <div className="header-text">
+                  <h3 className="main-text">
+                    Door to Door <span>delivery</span> services for individuals
+                    and businesses
+                  </h3>
+                </div>
+                <form className="main-form">
+                  <div className="pickup-location-input">
+                    <img src={pickupicon} alt="" className="left-icon" />
+                    {/* <Autocomplete> */}
+                    <input
+                      type="text"
+                      placeholder="Enter Pickup Location"
+                      className="input-main"
+                    />
+                    {/* </Autocomplete> */}
+                    <img src={locator} alt="" className="right-icon" />
+                  </div>
+                  <div className="delivery-location-input">
+                    <img src={dropofficon} alt="" className="left-icon" />
+                    {/* <Autocomplete> */}
+                    <input
+                      type="text"
+                      placeholder="Enter Delivery Location"
+                      className="input-main"
+                    />
+                    {/* </Autocomplete> */}
+                  </div>
+                  <div className="pickup-btn">
+                    <Link to="/join">
+                      <button className="pickup-btn" type="submit">
+                        Request Pickup
+                      </button>
+                    </Link>
+                  </div>
+                </form>
+                <img src={greendots} alt="" className="green-dots2" />
               </div>
-              <form className="main-form">
-                <div className="pickup-location-input">
-                  <img src={pickupicon} alt="" className="left-icon" />
-                  {/* <Autocomplete> */}
-                  <input
-                    type="text"
-                    placeholder="Enter Pickup Location"
-                    className="input-main"
-                  />
-                  {/* </Autocomplete> */}
-                  <img src={locator} alt="" className="right-icon" />
-                </div>
-                <div className="delivery-location-input">
-                  <img src={dropofficon} alt="" className="left-icon" />
-                  {/* <Autocomplete> */}
-                  <input
-                    type="text"
-                    placeholder="Enter Delivery Location"
-                    className="input-main"
-                  />
-                  {/* </Autocomplete> */}
-                </div>
-                <div className="pickup-btn">
-                  <Link to="/join">
-                    <button className="pickup-btn" type="submit">
-                      Request Pickup
-                    </button>
-                  </Link>
-                </div>
-              </form>
-              <img src={greendots} alt="" className="green-dots2" />
+            </div>
+            <div className="main-right-side">
+              <div className="delivery-picture">
+                <img src={image1} alt="" className="big-img" />
+                <img src={image2} className="small-img" />
+              </div>
+              <img src={reddots} alt="" className="red-dots2 " />
             </div>
           </div>
-          <div className="main-right-side">
-            <div className="delivery-picture">
-              <img src={image1} alt="" className="big-img" />
-              <img src={image2} className="small-img" />
-            </div>
-            <img src={reddots} alt="" className="red-dots2 " />
-          </div>
+          <Section2 />
         </div>
-        <Section2 />
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
