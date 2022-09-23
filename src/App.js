d96659fe8c325f00402ead134247d7b0d2dc4700
shadowpaka <jefferyehis.je@gmail.com>
2022-09-23 15:36:56 -0700

    forgot

--- src/App.js
+++ src/App.js
@@ -148,19 +148,8 @@ export default function App() {
               <Route path="fleet-v" element={<FleetVehicle />} />
               <Route path="account" element={<AgentCompleted />} />
               <Route path="success" element={<Success />} />
-<<<<<<< HEAD
-              <Route
-                path="user"
-                element={
-                  // <ProtectedRoutes></ProtectedRoutes>
-                  <LoggedinMainPageUser />
-                }
-              >
-                {/* The Route*/}
-=======
               <Route path="user" element={<LoggedinMainPageUser />}>
                 {/* The Route */}
->>>>>>> 1e7f1a79d833a1a9523ff7bbd5fc7955dd657df1
                 <Route path="agentlist" element={<AgentList />} />
                 <Route path="type" element={<DeliveryType />} />
                 <Route path="notifications" element={<Notification />} />
